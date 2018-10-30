5311926f8afe06c1e9cdee254e6fa90b6f05cb86
emiliano.fiorentini <emilianofiorentini@gmail.com>
2018-10-30 14:20:23 +0100

    Release 0.1.1

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import sklearn
+from sklearn.metrics import confusion_matrix
 import QueryClassifier as qc
 import NeuralNetwork as nn
 import Tools as tl
@@ -9,13 +10,17 @@ trainSet, trainLabels, testSet, testLabels = tl.getDatasets()
 # Create neural network
 neuralNetwork = nn.create_neural_network(len(qc.attack_keywords))
 # Train neural network
-neuralNetwork.fit(trainSet, trainLabels, epochs=75)
+neuralNetwork.fit(trainSet, trainLabels, epochs=20)
 print neuralNetwork.summary()
 # Test neural network
 predictions = neuralNetwork.predict(testSet)
 # Comparison between predictions and ground truth
 j = 0
+arg_max = np.zeros(len(predictions), dtype=int)
 for k in range(len(predictions)):
+    arg_max[k] = np.argmax(predictions[k])
     if np.argmax(predictions[k]) == testLabels[k]:
         j += 1
+cm = confusion_matrix(testLabels, arg_max)
+print cm, cm[0][0]
 print j, len(predictions)
