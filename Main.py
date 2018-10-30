55825b25a5af251abf1fccee768efe9c3ecc783d
EmanueleRaso-Notebook <emanuele.raso@gmail.com>
2018-10-30 11:12:35 +0100

    Primo tentativo main3

--- Main.py
+++ Main.py
@@ -1,18 +1,14 @@
 import numpy as np
-import random
-
-import sys
 
 import QueryGenerator as qg
 import QueryClassifier as qc
-#import NeuralNetwork as nn
+import NeuralNetwork as nn
 
 trainSet_dim = 0.7
-testSet_dim = 0.3
 
 def splitSet(dataSet):
     trainSet = np.zeros(((int)(trainSet_dim * len(dataSet)), len(qc.attack_keywords)),dtype=int)
-    testSet = np.zeros(((int)(testSet_dim * len(dataSet)), len(qc.attack_keywords)), dtype=int)
+    testSet = np.zeros((len(dataSet)-len(trainSet), len(qc.attack_keywords)), dtype=int)
     j = 0
     for i in range(len(dataSet)):
         if(i<(int)(trainSet_dim * len(dataSet))):
@@ -20,16 +16,26 @@ def splitSet(dataSet):
         else:
             testSet[j] = dataSet[i]
             j += 1
-    print dataSet.shape, trainSet.shape, testSet.shape
+    print len(dataSet), len(trainSet), len(testSet)
     print "TRAINSET:-----------------\n", trainSet
     print "TESTSET:-----------------\n", testSet
+    return trainSet, testSet
 
+# Read from files
 legitStrings, maliciousStrings = qg.load_csvFiles()
+# Create SQLI queries
 legitSet, maliciousSet = qg.generate_dataset(legitStrings, maliciousStrings)
-
+# Classify queries
 classifiedLegits = qc.classify(legitSet)
-legitLabels = np.zeros(len(classifiedLegits), dtype=int)
 classifiedMaliciouses = qc.classify(maliciousSet)
-maliciousLabels = np.ones(len(classifiedMaliciouses), dtype=int)
+# Create train and test sets and labels for neural network
+legitTrainSet, legitTestSet = splitSet(classifiedLegits)
+legitTrainLabels = np.zeros((int)(trainSet_dim*len(classifiedLegits)), dtype=int)
+maliciousTrainSet, maliciousTestSet = splitSet(classifiedMaliciouses)
+maliciousTrainLabels = np.ones((int)(trainSet_dim*len(classifiedMaliciouses)), dtype=int)
+# Merge two train sets and label sets
+trainSet = np.append(legitTrainSet, maliciousTrainSet)
+trainLabels = np.append(legitTrainLabels, maliciousTrainLabels)
 
-splitSet(classifiedLegits)
+neuralNetwork = nn.create_neural_network(qc.attack_keywords)
+neuralNetwork.f
